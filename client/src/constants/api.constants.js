792c22db5ce9fc35ef3b61ac4f625286fe00096d
Aron Bergman <brgmn@icloud.com>
2020-05-12 20:01:20 +0300

    Add api link

--- client/src/constants/api.constants.js
+++ client/src/constants/api.constants.js
@@ -1,15 +1,13 @@
-
 const API_URL = 'https://bergman.bar/api';
 
 export const API_CREATE_RENT_ADS = `${API_URL}/rent/create-ad`
-export const API_FETCH_ALL_RENT_ADS = `${API_URL}/rent/fetch-all`
+export const API_FETCH_ALL_RENT_ADS = `api/rent/fetch-all`
 export const API_FETCH_OFFSET_RENT_ADS = `${API_URL}/rent/fetch-offset`
 export const API_FETCH_USER_RENT_ADS = `${API_URL}/rent/fetch-user`
 export const API_FETCH_SINGLE_RENT_AD = `${API_URL}/rent/fetch-single-ad`
 export const API_FETCH_LOAD_FILES = `${API_URL}/upload`
 export const API_FETCH_DELETE_AD = `${API_URL}/rent/delete`
 export const API_FETCH_DELETE_AD_AUTH = `${API_URL}/rent/delete-auth`
-
 
 export const API_FETCH_DATING_CATEGORIES = `${API_URL}/dating/categories`
 export const API_FETCH_SINGLE_CATEGORY = `${API_URL}/dating/single-category`
