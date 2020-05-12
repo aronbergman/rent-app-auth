70d8a9326f4007e4b890c21c9686206f460acde5
Aron Bergman <brgmn@icloud.com>
2020-05-12 18:40:32 +0300

    server edit

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ if (process.env.NODE_ENV === 'production') {
         badge: true
     })
     app.use('/', express.static(path.join(__dirname, 'client', 'build')))
-    app.get('/', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
